d27638424a80088c9d2519c37e44eb1a50bfed21
raphaelmstudios <r.musau@alustudent.com>
2025-10-27 16:02:56 +0200

    Changes

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if 97 <= ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end='')
-        else:
-            print("{}".format(c), end='')
-    print()
+    def uppercase(str1):
+    for char in str1:
+        if 123 > ord(char) > 96:
+            char = chr(ord(char) - 32)
+        print('{}'.format(char), end="")
+    print('')
---
+++ python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This project directory shows a series of files and excercises I used the if , elif and the loops to solve the different scenarios
